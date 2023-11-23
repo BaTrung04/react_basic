d61fb655826534363885c2fb204e9aa8a81dffae
Ba Trung <batrung0410job@gmail.com>
2023-11-23 11:12:59 +0700

    Cau hinh redux

---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,11 @@
+const initState = {
+    users: [
+        { id: 1, name: "BaTrung" },
+        { id: 2, name: "Van a" },
+    ],
+};
+
+const rootReducer = (state = initState, active) => {
+    return state;
+};
+export default rootReducer;
